929dee7bcfeb684706a1d9ac4a5c5c19addc0d2c
root <root@ip-172-31-41-115.ap-northeast-2.compute.internal>
2022-02-04 12:51:10 +0000

    Add filterVod.php

--- filterVod.php
+++ filterVod.php
@@ -2,38 +2,89 @@
 
 require_once 'connect.php';
 
-$filter_array = $_POST['filter'];
+$filter_category = $_POST['filter_category'];
+$filter_difficulty = $_POST['filter_difficulty'];
+$filter_time = $_POST['filter_time'];
 
-$filter_category = "";
-$filter_difficulty = "";
-$filter_time = "";
+$filter1 = "없음";
+$filter2 = "없음";
+$filter3 = "없음";
+$filter4 = "없음";
+$filter5 = "없음";
 
-if ($filter_array[0] != null){
-    $filter_category = $filter_array[0];
+$diff_filter1 = "입문";
+$diff_filter2 = "초급";
+$diff_filter3 = "중급";
+
+
+$category_array = explode(', ',$filter_category);
+$difficulty_array = explode(', ',$filter_difficulty);
+
+if (count($category_array) == 1){
+    $filter1 = $category_array[0];
+} elseif (count($category_array) == 2){
+    $filter1 = $category_array[0];
+    $filter2 = $category_array[1];
+} elseif (count($category_array) == 3){
+    $filter1 = $category_array[0];
+    $filter2 = $category_array[1];
+    $filter3 = $category_array[2];
+} elseif (count($category_array) == 4){
+    $filter1 = $category_array[0];
+    $filter2 = $category_array[1];
+    $filter3 = $category_array[2];
+    $filter4 = $category_array[3];
+} elseif (count($category_array) == 5){
+    $filter1 = $category_array[0];
+    $filter2 = $category_array[1];
+    $filter3 = $category_array[2];
+    $filter4 = $category_array[3];
+    $filter5 = $category_array[4];
 }
 
-if($filter_array[1] != null){
-    $filter_difficulty = $filter_array[1];
-}
-
-if($filter_array[2] != null){
-    $filter_time = $filter_array[2];
+if (count($difficulty_array) == 1){
+    $diff_filter1 = $difficulty_array[0];
+    $diff_filter2 = "없음";
+    $diff_filter3 = "없음";
+} elseif (count($difficulty_array) == 2){
+    $diff_filter1 = $difficulty_array[0];
+    $diff_filter2 = $difficulty_array[1];
+    $diff_filter3 = "없음";
+} elseif (count($difficulty_array) == 3){
+    $diff_filter1 = $difficulty_array[0];
+    $diff_filter2 = $difficulty_array[1];
+    $diff_filter3 = $difficulty_array[2];
 }
 
 //일단은 카테고리 필터만 ㄱㄱ 
 //sql 문 만들어야함
 
 $tmp_array = array();
+//if ($filter_difficulty == null || $filter_time == null){
 
 $sql2 = "SELECT * FROM vod_table V
 INNER JOIN user_table U ON V.user_id = U.user_id
-WHERE V.user_id = '$user_id'
+WHERE V.vod_category REGEXP ('$filter1|$filter2|$filter3|$filter4|$filter5') AND V.vod_difficulty REGEXP ('$diff_filter1|$diff_filter2|$diff_filter3')
 ORDER BY vod_id DESC";
+
+
+
+
+// $sql2 = "SELECT * FROM vod_table V
+// INNER JOIN user_table U ON V.user_id = U.user_id
+// WHERE V.vod_category REGEXP ('$filter1|$filter2|$filter3|$filter4|$filter5') AND V.vod_difficulty = '$filter_difficulty'
+// ORDER BY vod_id DESC";
+
+// $sql2 = "SELECT * FROM vod_table V
+// INNER JOIN user_table U ON V.user_id = U.user_id
+// WHERE V.vod_category LIKE '%$filter_category%'
+// ORDER BY vod_id DESC";
+
 $result2 = mysqli_query($conn, $sql2);
 
 if(mysqli_num_rows($result2) > 0){
     while($row2 = mysqli_fetch_assoc($result2)){
-        //$tmp_array = array();
+        //$tmp_array = array(); 
         array_push($tmp_array, array(
             'vod_difficulty'=>$row2['vod_difficulty'],
             'vod_thumbnail'=>$row2['vod_img_path'],
