d7e94e07afb09f4f562a0ddf7e1a906eea1bebfb
root <root@ip-172-31-41-115.ap-northeast-2.compute.internal>
2022-02-09 12:52:43 +0000

    Add Search(5) php files

---
+++ getSearchHistory.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once 'connect.php';
+
+$user_id = $_POST['user_id'];
+
+$tmp_array = array();
+
+$sql2 = "SELECT *, date_format(sh_cdate, '%m.%d') as cdate FROM search_table
+WHERE user_id = '$user_id'
+ORDER BY sh_id DESC";
+
+$result2 = mysqli_query($conn, $sql2);
+
+if(mysqli_num_rows($result2) > 0){
+    while($row2 = mysqli_fetch_assoc($result2)){
+        //$tmp_array = array();
+        array_push($tmp_array, array(
+            'search_text'=>$row2['sh_text'],
+            'search_date'=>$row2['cdate'],
+            'search_id'=>$row2['sh_id']
+        ));
+    }
+
+    $response = array("resultArray"=>$tmp_array, "isSuccess"=>true);
+    echo json_encode($response);
+
+} else {
+    $response = array("resultArray"=>$tmp_array, "isSuccess"=>false);
+    echo json_encode($response);
+}
+
+mysqli_close($conn);
+
+
+?>
