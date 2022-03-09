dc50a7ce44f8f4a92af58b1e865ac0fbe1a040e5
root <root@ip-172-31-41-115.ap-northeast-2.compute.internal>
2022-03-09 04:02:00 +0000

    Add live_create_alarm

--- sendOpenAlarm.php
+++ sendOpenAlarm.php
@@ -2,24 +2,13 @@
 
 require_once 'connect.php';
 
-
-$user_id = $_POST['user_id'];
 $live_id = $_POST['live_id'];
-$message = $_POST['message'];
-$live_score = $_POST['score'];
+$message = $_POST['live_title'];
 
-$live_title = $_POST['title'];
-$live_start_hour = $_POST['start_hour'];
-$live_start_minute = $_POST['start_minute'];
-$live_spend_time = $_POST['spend_time'];
-$live_calorie = $_POST['calorie'];
-$live_limit_join = $_POST['limit_join'];
-$live_material = $_POST['material'];
-
-
+//알림테이블 조회해서 해당 live에 알림신청한 사람들의 토큰값 가져오기
 $sql = "SELECT * FROM alarm_table A
 INNER JOIN user_table U ON A.user_id = U.user_id
-WHERE A.uploader_id = '$user_id' AND A.li_id = '$live_id'
+WHERE A.li_id = '$live_id'
 ORDER BY A.al_id";
 
 $result = mysqli_query($conn, $sql);
@@ -29,17 +18,15 @@ while($row = mysqli_fetch_assoc($result)){
     array_push($to_array,$row['user_token']);
 }
 
-
-
 $header = array("Content-Type:application/json", "Authorization:key=AAAAk2XjGqM:APA91bFcCD0e8kPYdL8NKw7U55gsNNMeg1aG05wO8lFpNySHxB_ateXQ_LoYITRnv5qfr7XTQO9fVpvnJHLUVFxu6FD7hyB9JO5KJOmmDPq98zhnJVNyLxbAdLPg-e4wJyrBKEr-XV81");
 $data = json_encode(array(
     "registration_ids"=>$to_array,
     "priority" => "high",
     "notification" => array(
         "click_action" => ".HomeActivity",
-        "title"   => "라이브 수정 알림",
-        "body" => "트레이너의 메시지 : ".$message),
-    "data"=> array("moveto" => 2)
+        "title"   => "라이브 방송 시작 알림",
+        "body" => $message."가 시작되었습니다/".$live_id),
+    "data"=> array("moveto" => "5", "live_id" =>$live_id)
         ));
 
 $ch = curl_init();
@@ -52,20 +39,20 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 $result = curl_exec($ch );
 curl_close( $ch );
 
-$sql_update = "UPDATE live_table L
-    INNER JOIN trainer_table T on L.user_id = T.user_id
-    SET li_title = '$live_title', li_start_hour = '$live_start_hour',
-    li_start_minute = '$live_start_minute',li_spend_time = '$live_spend_time',
-    li_calorie = '$live_calorie', li_limit_join = '$live_limit_join',
-    li_material = '$live_material', tr_score = tr_score - $live_score
-    WHERE li_id = '$live_id'";
+$sql_update = "UPDATE live_table SET li_open = 1 WHERE li_id = '$live_id'";
 
-if (mysqli_query($conn,$sql_update)){
+$result_update = mysqli_query($conn, $sql_update);
+if ($result_update)
+{
     $response['success'] = true;
-
-} else {
-    $response['success'] = false;
+    $response['message'] = "추가 완료";
 }
+else
+{
+    $response['success'] = false;
+    $response['message'] = "추가 실패";
+}
+
 
 echo json_encode($response);
 
